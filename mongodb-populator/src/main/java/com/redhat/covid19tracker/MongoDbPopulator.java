426edb6c0d7becf53482682ce6e666b08749d289
Jochen Cordes <jochen@redhat.com>
2020-04-09 10:01:44 +0200

    Populating MongoDB from Kafka. Created logic for resolving current data for JHU CSSE data extraction.

--- mongodb-populator/src/main/java/com/redhat/covid19tracker/MongoDbPopulator.java
+++ mongodb-populator/src/main/java/com/redhat/covid19tracker/MongoDbPopulator.java
@@ -1,19 +1,32 @@
 package com.redhat.covid19tracker;
 
+import javax.inject.Inject;
 import javax.enterprise.context.ApplicationScoped;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import com.mongodb.client.MongoClient;
+
 @ApplicationScoped
 public class MongoDbPopulator extends RouteBuilder {
+    
+    @Inject MongoClient mongoClient;
 
-    @ConfigProperty(name = "kafka.brokers") 
-    String brokers;
+    @ConfigProperty(name = "camel.kafka.brokers")
+    private String brokers;
 
+    @ConfigProperty(name = "camel.mongodb.database")
+    private String database;
+
+    @ConfigProperty(name = "camel.mongodb.collection")
+    private String collection;
+
+    
     public void configure() {
-        from("kafka:jhucsse?brokers="+brokers)
-        .log("message: ${body}");
+        fromF("kafka:jhucsse?brokers=%s",brokers)
+        .log("message: ${body}")
+        .toF("mongodb:mongoClient?database=%s&collection=%s&operation=insert", database, collection);
     }
 }
 
