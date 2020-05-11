140491e928fe9101c6257707cb271f23e691fad8
Grant McCarthy <grantmc99@gmail.com>
2020-05-11 13:42:12 -0400

    renamed database to covid19report

--- covid-19-monitor/src/main/java/org/acme/DailyReport.java
+++ covid-19-monitor/src/main/java/org/acme/DailyReport.java
@@ -7,7 +7,7 @@ import org.bson.types.ObjectId;
 import io.quarkus.mongodb.panache.MongoEntity;
 import io.quarkus.mongodb.panache.PanacheMongoEntity;
 
-@MongoEntity(collection="covid19-report")
+@MongoEntity(collection="covid19report")
 public class DailyReport extends PanacheMongoEntity{
 	public ObjectId id;
 	public String country;
